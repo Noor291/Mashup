3fc9996d4ac3029787fb206f21ad77a86335ea85
Noor <78134535+Noor291@users.noreply.github.com>
2023-02-12 22:38:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,41 +14,30 @@ import os
 st.set_page_config("Mashup ❄️")
 st.title("Mashup ❄️")
 
-def download_videos(n, x):
-    # Create a directory to store the videos
-    if not os.path.exists("Videos"):
-        os.mkdir("Videos")
+def download_audio_from_search(singer, n, m):
+    results = YoutubeSearch(singer, max_results=m).to_dict()
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+    for i, result in enumerate(results):
+        video_url = "https://www.youtube.com/watch?v=" + result["id"]
+#         try:
+        yt = YouTube(requests.get(video_url, headers=headers).text)
+        stream = yt.streams.filter(only_audio=True).first()
+        stream.download(filename=f"{singer}_{i}.mp4")
 
-    # Search for videos of the singer
-    query = x + " songs"
-    s = Search(query)
-    searchResults = {}
-    i = 0
-    for v in s.results:
-        if i <n and v.length < 1200:
-            try:
-                searchResults[v.title] = v.watch_url
-                youtubeObject = YouTube(v.watch_url)
-                youtubeObject = youtubeObject.streams.get_highest_resolution().download(
-                    output_path='Videos', filename=f"video{i+1}.mp4")
-                print(f"Downloaded video {i + 1}: {v.title}")
-                i = i+1
-            except:
-                print("error occured")
+        audio = pydub.AudioSegment.from_file(f"{singer}_{i}.mp4")
+        audio = audio[:n * 1000]
+        audio.export(f"{singer}_{i}.mp3", format="mp3")
+        
+#         except:
+        time.sleep(3)
 
-
-def convertToAudio(duration, n):
-    if not os.path.exists("Audios"):
-        os.mkdir("Audios")
-    for i in range(n):
-        clip = mp.VideoFileClip(f"Videos/video{i+1}.mp4").subclip(0, duration)
-        clip.audio.write_audiofile(f"Audios/audio{i+1}.mp3")
-
-def makeMashup(n):
-    audio_clips = [mp.AudioFileClip(
-        f"Audios/audio{i+1}.mp3") for i in range(0, n)]
-    final_clip = mp.concatenate_audioclips(audio_clips)
-    final_clip.export(f"combined.mp3", format="mp3")
+def combine_audio_files(singer, n, m):
+    combined = AudioSegment.empty()
+    for i in range(m):
+        audio = AudioSegment.from_file(f"{singer}_{i}.mp3")
+        combined += audio
+    combined.export(f"combined.mp3", format="mp3")
 
 def sendMail(recp):
     # Email details
@@ -81,11 +70,17 @@ m = st.number_input("Duration of each Audio Clip (in seconds):", min_value=20, m
 recp = st.text_input("Email ID:")
 bt1 = st.button("Send Mail")
 if bt1:
-    with st.spinner('Preparing Audio... (this may take upto a few minutes)'):
+    # if download_audio_from_search(singer, n, m):
+    # combine_audio_files(singer, n, m)
+    # recp = st.text_input("Enter receiver email:")
+    with st.spinner('Preparing Audio... this may take upto a few minutes'):
+#         download_audio_from_search(singer, n, m)
+#         combine_audio_files(singer, n, m)
+#         sendMail(recp)
+#         st.success('Your file was mailed successfully!')
         try:
-            download_videos(n,singer)
-            convertToAudio(m,n)
-            makeMashup(n)
+            download_audio_from_search(singer, n, m)
+            combine_audio_files(singer, n, m)
             sendMail(recp)
             st.success('Your file was mailed successfully!')
         except:
